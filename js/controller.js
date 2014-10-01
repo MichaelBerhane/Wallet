3fffad60a5e7b195f5533b842d500a54c5774cb4
Michael Berhane <michael@michaelberhane.org>
2014-10-01 14:35:06 +0100

    able to increase and decrease total; data not saved locally

--- js/controller.js
+++ js/controller.js
@@ -8,25 +8,40 @@ var appControllers = angular.module('appControllers', []);
   angular.module('myApp').factory('Wallet', function(){
 
     var balance = 0.00;
+    var currency = "gpb";
 
     return {
         check_amount: function(){
           return balance;
         },
-        add_amount: function(amount){
-          balance += amount;
+        add_amount: function(field){
+          balance += field;
+          console.log(balance);
+          return this.check_amount();
         },
         minus_amount: function(amount){
-          if(this.correct_amount(amount))
+
             balance -= amount;
-          else
-            return false
+            console.log(balance + ": minus");
+            return this.check_amount();
         },
         correct_amount: function(amount){
           if((balance -= amount) > 0 )
             return false;
           else
             return true;
+        },
+        change_currency: function(selected){
+          if(selected == 'USD'){
+            currency = "usd";
+          }
+          else if(selected == 'EUR'){
+            currency = "eur";
+          }
+          return currency;
+        },
+        currency: function(){
+          return currency;
         },
         reset: function(){
           balance = 0.00;
@@ -41,7 +56,26 @@ var appControllers = angular.module('appControllers', []);
 
   appControllers.controller('WalletController', ['$scope', 'Wallet', function($scope, Wallet){
 
+    $scope.gbpcurrency = true;
+    $scope.usdcurrency = false;
+    $scope.eurcurrency = false;
+    $scope.field = 0.00;
+    $scope.Minusfiled = 0.00;
+    $scope.items = [{ id: 1, curr: 'USD' },
+        { id: 2, curr: 'GBP' },
+        { id: 3, curr: 'EUR'}];
+
     $scope.balance = Wallet.check_amount();
+
+    $scope.add = function(){
+
+      $scope.balance = Wallet.add_amount(Number($scope.field));
+    };
+
+    $scope.minus = function(){
+      $scope.balance = Wallet.minus_amount(Number($scope.Minusfield));
+    };
+
 
 
   }]);
